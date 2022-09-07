9ad43c05617c138a5cdd28c436b4b75465b40602
minghua wang <minghua1993@gmail.com>
2022-09-07 12:34:29 -0400

    minghua

--- server/server.js
+++ server/server.js
@@ -1,41 +1,26 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-const { OAuth2Client } = require('google-auth-library');
+const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const { MONGO_URI } = process.env;
 
-dotenv.config();
-const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
-
-const app = express();
-app.use(express.json());
-
-const users = [];
-
-function upsert(array, item) {
-    const i = array.findIndex((_item) => _item.email === item.email);
-    if (i > -1) array[i] = item;
-    else array.push(item);
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }
 
-app.post('/api/google-login', async (req, res) => {
-    const { token } = req.body;
-    const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.CLIENT_ID,
-    });
-    const { name, email, picture } = ticket.getPayload();
-    upsert(users, { name, email, picture });
-    res.status(201);
-    res.json({ name, email, picture });
-});
+const dbFunction = async (dbName) => {
+    const client = new MongoClient(MONGO_URI, options);
 
-app.use(express.static(path.join(__dirname, '/build')));
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/build/index.html'))
-);
+    await client.connect();
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(
-    `Server is ready at http://localhost:${process.env.PORT || 8000}`
-    );
-});
+    const db = client.db(dbName);
+    console.log('connected!');
+
+    const users = await db.collection("users").find().toArray();
+    console.log(users);
+
+    client.close();
+    console.log("disconnected!");
+};
+
+dbFunction("minghua");
